11ea68df954dd900beddea4f8fdf89fe4fbae8c9
Chipmunk_G4 <eunchan9029@postech.ac.kr>
2020-07-29 18:48:14 +0900

    아싸 퇴근이당 (roomlist 작업중)

--- code/carpool-backend/routes/roomlist.js
+++ code/carpool-backend/routes/roomlist.js
@@ -41,14 +41,43 @@ router.get('/', function(req, res, next) {
     // 2. DB.query로 보내서 출력 가져오기
     DB.query(sql,(err,rows,fields)=>{
         if (err) throw err;
-        res.json(rows) // 3. 사용자에게 json 형식으로 출력하기
         console.log(rows)
+
+        res.json(rows) // 3. 사용자에게 json 형식으로 출력하기
+        res.status(200).end()
     })
 });
 
 // POST /roomlist
+// (필수적으로) 입력 되는 값: car_type, depart_place, arrive_place, depart_time, arrive_time
 router.post('/',(req,res)=>{
-    res.send('POST /roomlist');
+    
+    console.log(req.body)
+    car_type = req.body.car_type
+    depart_place = req.body.depart_place
+    arrive_place = req.body.arrive_place
+    depart_time = req.body.depart_time
+    arrive_time = req.body.arrive_time    
+
+    // 시간 검사용 정규표현식
+    var regExp = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/;
+
+    // 오류처리
+    if(car_type != "자가용" || car_type != "택시" || depart_place == undefined || arrive_place == undefined || depart_time == undefined || arrive_time == undefined){
+        res.status(400)
+    }
+
+    // 1. MySql query문 만들기
+    var property = `'${car_type}','${depart_place}','${arrive_place}','${depart_time}','${arrive_time}'`
+    //var property = `'자가용','지곡회관','공학동',NOW(),NOW()` //시간: 2020-07-29 14:10:23 형태를 한다
+    var sql = `INSERT INTO roominfos (car_type, depart_place, arrive_place, depart_time, arrive_time,current_headcount, total_headcount, curreunt_carrier_num, total_carrier_num, userId, isConfirm, confirm_time) VALUES(${property},0,4,0,4,0,0,NOW())`;
+    
+    // 2. DB.query로 보내서 출력 가져오기
+    DB.query(sql,(err,rows,fields)=>{
+        if (err) throw err;      
+    })
+    res.redirect(200, '/')
+    res.end()
 });
 // PUT /roomlist
 router.put('/',(req,res)=>{
